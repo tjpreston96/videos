53b3272dcfd7ebdd4938977f9f196d1304e38a1a
tpreston96 <tpreston96@gmail.com>
2021-02-21 16:27:28 -0600

    defaulting video search / finished code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,13 +6,20 @@ import VideoDetail from "../VideoDetail/VideoDetail";
 class App extends Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit("React development");
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -25,8 +32,10 @@ class App extends Component {
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
+            <div className="ten wide column">
+              <div className="ui segment">
+                <VideoDetail video={this.state.selectedVideo} />
+              </div>
             </div>
             <div className="five wide column">
               <VideoList
