9babe10990da1e6cfa482aef5e12329f8f4989ed
tpreston96 <tpreston96@gmail.com>
2021-02-20 23:56:54 -0600

    configure response

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,20 +3,22 @@ import SearchBar from "../SearchBar/SearchBar";
 import youtube from "../../apis/youtube";
 
 class App extends Component {
-  state = {};
+  state = { videos: [] };
 
-  onTermSubmit = (term) => {
-    youtube.get("/search", {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos
       </div>
     );
   }
