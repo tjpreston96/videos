4ea681228a42d32cffd8265c9ed43cffec0e10c9
tpreston96 <tpreston96@gmail.com>
2021-02-20 23:05:30 -0600

    connect SearchBar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 
 class App extends Component {
-    state = {  }
-    render() { 
-        return ( <div>Hi there!</div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
- 
+
 export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+    state = {  }
+    render() { 
+        return ( <div>Search Bar</div> );
+    }
+}
+ 
+export default SearchBar;
